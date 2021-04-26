0f38d25c9f905101e2ecd6f3b758408a31acbcb3
Andy Schachter <andy.schachter@gmail.com>
2021-04-26 19:28:15 -0400

    added framework of all teams get request and all request

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const teams = require('./teams')
 
 const app = express()
 
+app.get('/Teams', (request, response) => {
+
+})
+
+app.all('*', (request, response) => {
+  return response.sendStatus(404)
+})
+
 app.listen(1342, () => {
   console.log('listening on port 1342...') // eslint-disable-line no-console
 })
