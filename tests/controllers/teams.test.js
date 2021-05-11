c00183b33ae88f3bdeee098599768276c5da1852
Andy Schachter <andy.schachter@gmail.com>
2021-05-10 21:01:51 -0400

    added dummy list to teams.js in mocks, added tests for getAllTeams in teams.tests.js

--- tests/controllers/teams.test.js
+++ tests/controllers/teams.test.js
@@ -4,6 +4,7 @@ const sinonChai = require('sinon-chai')
 const models = require('../../models')
 const { describe, it } = require('mocha')
 const { getAllTeams, getTeam, addNewTeam, getTeamByDiv } = require('../controller/teams')
+const { teamsList } = require('../mocks/teams')
 
 chai.use(sinonChai)
 const { expect } = chai
@@ -12,6 +13,13 @@ describe('Teams Controller', () => {
   describe('Get All Teams', () => {
     it('returns list of all teams in database calling response.send() for the specified list', async () =>{
       const stubbedFindAll = sinon.stub(models.teams, 'findAll').returns(teamsList)
+      const stubbedSend = sinon.stub()
+      const response = { send: stubbedSend }
+
+      await getAllTeams({}, response)
+
+      expect(stubbedFindAll).to.have.callCount(1)
+      expect(stubbedFindAll).to.have.been.calledWith(teamsList)
     })
 
   })
